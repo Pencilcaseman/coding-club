02d4fc1feb90ead99912734ded271a7e01d587c8
Pencilcaseman <pencilcaseman@gmail.com>
2021-12-01 13:45:49 +0000

    Implemented piece placing functionality

--- Othello/sketch.js
+++ Othello/sketch.js
@@ -14,33 +14,39 @@ let cellHeight = pixelHeight / cellsY; // Pixel height of block
 function initializeBoard() {
   board = []
   // Initialise the board and fill it with zeros
-  for (let i = 0; i < cellsY; i++) {
+  for (let i = 0; i < cellsX; i++) {
     board[i] = [];
-    for (let j = 0; j < cellsX; j++) {
-      board[i][j] = empty;
+    for (let j = 0; j < cellsY; j++) {
+      board[i][j] = 0;
     }
   }
+}
+
+// Place a piece given the screen-space coordinate X and Y
+function placePiece(coordX, coordY) {
+  // Find the index in the board
+  pieceX = floor(coordX / pixelWidth);
+  pieceY = floor(coordY / pixelHeight);
 }
 
 // Draw the board to the screen
 function drawBoard() {
   // Draw each cell as a square with an outline
-  for (let i = 0; i < cellsY; i++) {
-    for (let j = 0; j < cellsX; j++) {
+  for (let i = 0; i < cellsX; i++) {
+    for (let j = 0; j < cellsY; j++) {
       fill(50, 170, 50);
-      stroke(0, 0, 0);
+      stroke(0);
       strokeWeight(2.5);
-      // rect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
       rect(i * cellWidth, j * cellHeight, cellWidth, cellHeight);
       
       noStroke();
-      if (board[i][j] === p1) {
+      if (board[j][i] == p1) {
         fill(255);
-      } else if (board[i][j] === p2) {
+      } else if (board[j][i] == p2) {
         fill(0);
       }
       
-      if (board[i][j] !== empty) {
+      if (board[j][i] != empty) {
         circle((i + 0.5) * cellWidth, (j + 0.5) * cellHeight, cellWidth / 1.5);
       }
     }
@@ -48,7 +54,7 @@ function drawBoard() {
 }
 
 function setup() {
-  createCanvas(pixelWidth, pixelHeight);
+  createCanvas(400, 400);
   initializeBoard();
   
   board[floor(cellsY / 2)][floor(cellsX / 2)] = p1;
@@ -61,4 +67,10 @@ function draw() {
   background(220);
   
   drawBoard();
+}
+
+function mousePressed() {
+  row = floor(mouseX / cellWidth);
+  col = floor(mouseY / cellHeight);
+  board[col][row] = 1;
 }
